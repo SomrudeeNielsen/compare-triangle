5cc0847c4a0f3ffd08af76b358c5209fbf65701f
Somrudee Nielsen <Ingtawan05@gmail.com>
2021-12-02 14:11:10 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//const triangleArea = (base*height)/2
+//case-one
+let triangleOneArea = (10*13)/2
+console.log (triangleOneArea);
+
+let triangleTwoArea = (16.5*20.3)/2
+console.log(triangleTwoArea);
+
+console.log(triangleOneArea < triangleTwoArea);
+
+const caseOne =  `triangleOne area is ${triangleOneArea} cm2,
+triangleTwo area is ${triangleTwoArea} cm2. The triangle with the biggest area is triangleTwo.`
+console.log(caseOne);
+
+//case-two
+let triangleThreeArea = (16.5*20.3)/2
+console.log (triangleThreeArea);
+
+let triangleFourArea = (20.3*16.5)/2
+console.log(triangleFourArea);
+
+console.log(triangleThreeArea > triangleFourArea);
+
+const caseTwo =  `triangleThree area is ${triangleThreeArea} cm2,
+triangleFour area is ${triangleFourArea} cm2. Both triangles have the same amout of the area.`
+console.log(caseTwo);
+
+//case-three
+let triangleFiveArea = (7.8*5.6)/2
+console.log (triangleFiveArea);
+
+let triangleSixArea = (9.3*12.4)/2
+console.log(triangleSixArea);
+
+console.log(triangleFiveArea < triangleSixArea);
+
+const caseThree =  `triangleFive area is ${triangleFiveArea} cm2,
+triangleSix area is ${triangleSixArea} cm2. The triangle with the biggest area is triangleSix.`
+console.log(caseTwo);
